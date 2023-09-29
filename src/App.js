f2a1ebf630d3e59b808248ed8905cd044c2c8643
alanbacay02 <alan.social02@gmail.com>
2023-09-29 15:20:52 +0800

    Imports Footer.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Hero from './components/Hero'
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
   <div>
     <Navbar />
     <Hero />
+    <Footer />
   </div>
   );
 }
