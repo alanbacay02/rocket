81317b5741d48be84e7bfd86d8bcbebd318ee9c1
alanbacay02 <alan.social02@gmail.com>
2023-09-29 15:11:52 +0800

    Adds custom classes for website color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // default: '#FFFFFF',
+        // primary: '#1D2A8C',
+        // secondary: '#2B2B31',
+        // accent: '#FDE217',
+        default: '#FAF7E0',
+        primary: '#112aea',
+        primaryText: '#919eff',
+        secondary: '#1D1B1D',
+        accent: '#F41AF4',
+        background: '#0E0F15'
+      }
+    },
   },
   plugins: [],
 }
-
