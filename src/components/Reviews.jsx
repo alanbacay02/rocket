b5fa61075c4a708f834ca5bec57e27ac9bb88c0d
alanbacay02 <alan.social02@gmail.com>
2023-10-12 17:34:59 +0800

    Adds basic header and p element to Reviews. Creates component ReviewCarousel and imports it to Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ReviewCarousel from './ReviewCarousel'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <div className='w-full py-12'>
+      <div className='w-full max-w-[1440px] mx-auto'>
+        <div className='flex flex-col justify-center items-center text-default'>
+          <h1 className='font-bold text-4xl'>Testimonials</h1>
+          <p className='text-2xl mt-4'>What other people say about us</p>
+        </div>
+
+        <ReviewCarousel />
+      </div>
+    </div>
   )
 }
 
