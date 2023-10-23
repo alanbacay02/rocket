dfe094209bfda2dfb84017b41734629bc3778156
alanbacay02 <alan.social02@gmail.com>
2023-10-23 15:38:19 +0800

    Creates a component NavPopMenu.jsx to act as popout menu when hamburger icon is clicked on Navbar.jsx

---
+++ src/components/NavPopMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavPopMenu = () => {
+  return (
+    <div>NavPopMenu</div>
+  )
+}
+
+export default NavPopMenu
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Logo5, Pin } from '../svg-components/SvgLogos'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import NavPopMenu from './NavPopMenu'
 
 const NAVBAR_ITEMS = [
   {
@@ -33,32 +34,39 @@ const Navbar = () => {
   const [menu, setMenu] = useState(false)
 
   return (
-    <div className='absolute inset-0 h-fit w-full py-4 md:py-6 border-b border-gray-800 z-50'>
-      <div className='w-full max-w-[1440px] px-2 sm:px-4 md:px-6 mx-auto flex flex-row justify-between items-center'>
-        <a href='/#' className='hover:cursor-pointer'>
-          <div className='flex flex-row justify-center items-center gap-0.5'>
-            <div className='w-7 h-7 md:w-11 md:h-11 mt-1 fill-primary'>
-              <Logo5 />
+    <div className='relative'>
+      <div className='absolute inset-0 h-fit w-full py-4 md:py-6 border-b border-gray-800 z-50'>
+        <div className='w-full max-w-[1440px] px-2 sm:px-4 md:px-6 mx-auto flex flex-row justify-between items-center'>
+          <a href='/#' className='hover:cursor-pointer'>
+            <div className='flex flex-row justify-center items-center gap-0.5'>
+              <div className='w-7 h-7 md:w-11 md:h-11 mt-1 fill-primary'>
+                <Logo5 />
+              </div>
+              <h3 className='text-default font-bold text-xl md:text-3xl'>Orion</h3>
             </div>
-            <h3 className='text-default font-bold text-xl md:text-3xl'>Orion</h3>
+          </a>
+          <div className='hidden md:block'>
+            <ul className='flex flex-row gap-5 text-default text-lg overflow-clip'>
+              {NAVBAR_ITEMS.map((item, index) => {
+                return (
+                  <li className='relative group flex flex-row items-center gap-1 pl-6'>
+                    <div className='absolute -left-1.5 bottom-[-40px] group-hover:-bottom-2.5 fill-accent transition-all duration-300 w-10 h-10'>
+                      <Pin />
+                    </div>
+                    <a href={item.itemHref}>{item.itemTitle}</a>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
-        </a>
-        <div className='hidden md:block'>
-          <ul className='flex flex-row gap-5 text-default text-lg overflow-clip'>
-            {NAVBAR_ITEMS.map((item, index) => {
-              return (
-                <li className='relative group flex flex-row items-center gap-1 pl-6'>
-                  <div className='absolute -left-1.5 bottom-[-40px] group-hover:-bottom-2.5 fill-accent transition-all duration-300 w-10 h-10'>
-                    <Pin />
-                  </div>
-                  <a href={item.itemHref}>{item.itemTitle}</a>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-        <div className='md:hidden text-default text-2xl mr-2' onClick={() => {setMenu(!menu)}}>
-          <RxHamburgerMenu />
+          <div className='md:hidden text-default text-2xl mr-2' onClick={() => {setMenu(!menu)}}>
+            <RxHamburgerMenu />
+          </div>
+
+
+          <div className='absolute top-0 right-0'>
+            <NavPopMenu />
+          </div>
         </div>
       </div>
     </div>
