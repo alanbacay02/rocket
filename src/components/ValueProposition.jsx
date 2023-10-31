7a489c4116716115c0341e03507eea6b12b92bd5
alanbacay02 <alan.social02@gmail.com>
2023-10-31 16:40:49 +0800

    Makes component fully responsive to all devices

--- src/components/ValueProposition.jsx
+++ src/components/ValueProposition.jsx
@@ -21,23 +21,23 @@ const CARD_CONTENT = [
 
 const ValueProposition = () => {
   return (
-    <div className='relative w-full pt-20'>
-      <div className='w-full max-w-[1440px] mx-auto'>
+    <div className='relative w-full py-16'>
+      <div className='w-full max-w-[1440px] px-4 mx-auto'>
         {/* START OF FLEX CONTAINER */}
         <div className='flex flex-col justify-center items-center text-default'>
-          <h1 className='font-bold text-4xl'>Why Choose Orion</h1>
+          <h1 className='font-bold text-4xl text-center'>Why Choose Orion</h1>
           {/* START OF GRID */}
-          <div className='grid md:grid-cols-3 px-6 gap-20 mt-[120px]'>
+          <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-20 mt-20'>
             {CARD_CONTENT.map((item, index) => {
               return (
-                <div className='flex flex-col items-center w-full h-full rounded-2xl bg-secondary px-12 pb-10 -z-50'>
-                  <div className='h-auto w-64 -mt-24'>
+                <div className={`flex flex-col items-center h-full rounded-2xl bg-secondary px-4 md:px-12 py-10 -z-50 ${index === CARD_CONTENT.length - 1 ? 'sm:col-span-2 lg:col-span-1 sm:mx-auto lg:mx-0' : ''} max-w-[300px] sm:max-w-[330px] md:max-w-[400px] lg:w-auto`}>
+                  <div className='h-auto w-32 sm:w-44 md:w-56'>
                     {item.cardSvgLogo}
                   </div>
                   <div>
-                    <h2 className='card-h2-title relative font-medium text-default text-2xl mt-8 mb-7 z-50'>{item.cardTitle}</h2>
+                    <h2 className='card-h2-title relative font-medium text-default text-lg sm:text-xl md:text-2xl mt-8 mb-7 z-50'>{item.cardTitle}</h2>
                   </div>
-                  <p className='text-default text-lg leading-9'>{item.cardParagraph}</p>
+                  <p className='text-default md:text-lg leading-9'>{item.cardParagraph}</p>
                 </div>
               )
             })}
