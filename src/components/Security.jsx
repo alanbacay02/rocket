e1d38275cc85c66f81f6eb8ed842606bf57d1fc6
alanbacay02 <alan.social02@gmail.com>
2023-10-31 16:37:55 +0800

    Makes component fully responsive to all devices

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -27,26 +27,28 @@ const Security = () => {
         <WaveTop />
       </div>
 
-      <div className='w-full h-min max-w-[1440px] mx-auto -mt-32'>
-        <div className='grid grid-cols-2'>
-          <div className=''>
+      <div className='w-full h-fit max-w-[1440px] mx-auto my-14 md:my-0 xl:-mt-28'>
+        <div className='grid lg:grid-cols-2 items-center'>
+          <div className='hidden lg:block'>
             <img src={serverImg} alt='serverImg' />
           </div>
-          <div className='flex flex-col max-w-[600px] mx-auto items-center justify-center gap-5 px-6 text-default text-xl'>
-            <h2 className='text-3xl font-medium'>We Put Security First to Safeguard What Matters Most</h2>
+          <div className='flex flex-col max-w-[500px] lg:max-w-[600px] mx-auto items-center justify-center gap-5 px-4 text-default'>
+            <h2 className='text-xl sm:text-2xl md:text-3xl font-medium text-center lg:text-left'>We Put Security First to Safeguard What Matters Most</h2>
             {CARD_CONTENT.map((item, index) => {
               return (
-                <div className='flex flex-row items-center px-6 py-5 gap-6 bg-[#003097] shadow-xl rounded-xl'>
-                  <div className='h-auto w-32'>
+                <div className='w-fit flex flex-col xs:flex-row items-center px-3 md:px-6 py-5 gap-3 bg-[#003097] shadow-xl rounded-xl'>
+                  <div className='h-auto w-20 xs:w-24 md:w-32'>
                     {item.cardIcon}
                   </div>
-                  {item.cardDesc}
+                  <p className='text-base sm:text-lg md:text-xl text-center xs:text-left'>
+                    {item.cardDesc}
+                  </p>
                 </div>
               )
             })}
-            <div className='flex flex-row w-full justify-start items-center gap-5 pl-2'>
-              <button onClick={() => {}} className='px-6 py-2.5 rounded-3xl shadow-xl bg-accent hover:bg-accent/90 transition-colors'>Learn More</button>
-              <button onClick={() => {}} className='px-6 py-2.5 rounded-3xl shadow-xl bg-white hover:bg-white/90 text-primary'>Documentation</button>
+            <div className='flex flex-row w-full justify-center lg:justify-start items-center gap-3 md:gap-5 md:pl-2 text-sm sm:text-base md:text-lg lg:text-xl'>
+              <button onClick={() => {}} className='px-3.5 md:px-6 py-2.5 rounded-3xl shadow-xl bg-accent hover:bg-accent/90 transition-colors'>Learn More</button>
+              <button onClick={() => {}} className='px-3.5 md:px-6 py-2.5 rounded-3xl shadow-xl bg-white hover:bg-white/90 text-primary'>Documentation</button>
             </div>
           </div>
         </div>
